f9c21ecb17c64a4c023ed1cba9a03be8f22e03ec
Harsh <coolharshkumar4@gmail.com>
2021-02-25 22:28:03 +0530

    start and help added(first stage)

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ client = coc.login('kooolme444@gmail.com', '8299048747@love')
 @bot.message_handler(commands=['getPlayerBh'])
 def handle_command(message):
 
-    async def mainn():
+    async def getInfo():
 
         try:
             player_tag = message.text.split()[1]
@@ -18,12 +18,25 @@ def handle_command(message):
             bot.reply_to(message, f"Your BH level is {player.builder_hall}.")
 
         except IndexError:
-            bot.reply_to(message, f"Incomplete Command!")
+            bot.reply_to(message, f"Incomplete Command!, Make sure to put correct id with '#' start of it.")
 
+        except coc.errors.NotFound:
+            bot.reply_to(message, f"Not Found!, Make sure to put correct id with '#' start of it.")
+        
         except Exception:
             pass
             
-    client.loop.run_until_complete(mainn())
+    client.loop.run_until_complete(getInfo())
+
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_command(message):
+    bot.reply_to(message, f'''I am Clash Bot and can used to fetch data related to Clash of Clans!
+Here are some following commands available yet:-
+    
+/getPlayerBh #<Player Id> To get a player's BH level.''')
+
+
     
 bot.polling()
 
