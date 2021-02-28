48315d14de4cabad68e317b170b999eb82754753
Harsh <coolharshkumar4@gmail.com>
2021-02-28 21:44:51 +0530

    added try.py, getClan and getClanDetails command

--- bot.py
+++ bot.py
@@ -2,35 +2,13 @@ import telebot
 import coc
 from decouple import config
 
-from bot_message import start_message, help_message, clan_button_message, player_button_message
+import bot_message
 
 bot = telebot.TeleBot(config('BOTAPI',cast=str))
 
 client = coc.login(config('EMAIL', cast=str), config('PASS', cast=str))
 
-@bot.message_handler(commands=['getPlayerBh'])
-def handle_command(message):
-
-    async def getInfo():
-
-        try:
-            player_tag = message.text.split()[1]
-            player = await client.get_player(player_tag)
-
-            bot.reply_to(message, f"Your BH level is {player.builder_hall}.")
-
-        except IndexError:
-            bot.reply_to(message, f"Incomplete Command!, Make sure to put correct id with '#' start of it.")
-
-        except coc.errors.NotFound:
-            bot.reply_to(message, f"Not Found!, Make sure to put correct id with '#' start of it.")
-        
-        except Exception:
-            pass
-            
-    client.loop.run_until_complete(getInfo())
-
-
+#Bot related functions
 @bot.message_handler(commands=['start'])
 def handle_command(message):
 
@@ -39,7 +17,7 @@ def handle_command(message):
         keyboard = telebot.types.InlineKeyboardMarkup()
         help_button = telebot.types.InlineKeyboardButton('Help', callback_data='help')
         keyboard.add(help_button)
-        bot.send_message(message.chat.id, start_message, reply_markup=keyboard, parse_mode='HTML')
+        bot.send_message(message.chat.id, bot_message.start_message, reply_markup=keyboard, parse_mode='HTML')
     
     else:   #For group chat
         bot.reply_to(message, f"I am up!")
@@ -67,7 +45,7 @@ def callback_handler(call):
         player_button = telebot.types.InlineKeyboardButton('Players', callback_data='players')
         keyboard.add(clan_button, player_button)
 
-        bot.edit_message_text(help_message, call.message.chat.id, call.message.id, reply_markup=keyboard, parse_mode='HTML')
+        bot.edit_message_text(bot_message.help_message, call.message.chat.id, call.message.id, reply_markup=keyboard, parse_mode='HTML')
 
     elif call.data == 'clans':
 
@@ -75,7 +53,7 @@ def callback_handler(call):
         back_button = telebot.types.InlineKeyboardButton('Back', callback_data='help',)
         keyboard.add(back_button)
 
-        bot.edit_message_text(clan_button_message, call.message.chat.id, call.message.id, reply_markup=keyboard, parse_mode='HTML')
+        bot.edit_message_text(bot_message.clan_button_message, call.message.chat.id, call.message.id, reply_markup=keyboard, parse_mode='HTML')
     
     elif call.data == 'players':
 
@@ -83,7 +61,69 @@ def callback_handler(call):
         back_button = telebot.types.InlineKeyboardButton('Back', callback_data='help')
         keyboard.add(back_button)
 
-        bot.edit_message_text(player_button_message, call.message.chat.id, call.message.id, reply_markup=keyboard, parse_mode='HTML')
+        bot.edit_message_text(bot_message.player_button_message, call.message.chat.id, call.message.id, reply_markup=keyboard, parse_mode='HTML')
+
+
+#Clans related functions
+@bot.message_handler(commands=['getClan'])
+def handle_command(message):
+
+    async def getInfo():
+
+        try:
+            clan_tag = message.text.split()[1]
+            clan = await client.get_clan(clan_tag)
+
+            bot.send_photo(message.chat.id, clan.badge.url, bot_message.clan_message.format(clan.name, clan.level, clan.share_link), message.message_id)
+
+        except IndexError:
+            bot.reply_to(message, f"Incomplete Command! Make sure to put correct id with '#' start of it.")
+
+        except coc.errors.NotFound:
+            bot.reply_to(message, f"Not Found! Make sure to put correct id with '#' start of it.")
+        
+        except Exception:
+            pass
+    
+    client.loop.run_until_complete(getInfo())
+
+
+@bot.message_handler(commands=['getClanDetails'])
+def handle_command(message):
+
+    async def getInfo():
+
+        try:
+            clan_tag = message.text.split()[1]
+            clan = await client.get_clan(clan_tag)
+
+            bot.reply_to(message, bot_message.clan_details_message.format(
+                clan.name,
+                clan.level,
+                clan.description,
+                clan.location,
+                clan.points,
+                clan.versus_points,
+                clan.type,
+                clan.required_trophies,
+                clan.member_count,
+                clan.war_league,
+                clan.war_frequency,
+                clan.war_wins,
+                clan.war_losses,
+                clan.war_win_streak,
+            ))
+
+        except IndexError:
+            bot.reply_to(message, f"Incomplete Command! Make sure to put correct id with '#' start of it.")
+
+        except coc.errors.NotFound:
+            bot.reply_to(message, f"Not Found! Make sure to put correct id with '#' start of it.")
+        
+        except Exception:
+            pass
+    
+    client.loop.run_until_complete(getInfo())
 
 
 bot.polling()
--- bot_message.py
+++ bot_message.py
@@ -1,12 +1,12 @@
 start_message = '''
 <b>CLASH BOT</b>
 
-I am Clash Bot which can used to fetch Players, Clan and CWL data of Clash of Clans!
+I am <i><b>Clash Bot</b></i> which can used to fetch Players, Clan and CWL data of Clash of Clans!
 
 It's still under development! So feedbacks will be very helpful.
 
 Use '<b>Help</b>' button to get list of commands.
-Use '<b>Contact</b>' button to make contact with developer (Coming soon).
+Use '<b>Contact</b>' button to make contact with developer <i>(Coming Soon)</i>.
 '''
 
 help_message = '''
@@ -18,13 +18,17 @@ We have three sections-
 
 <b>Clans</b> - This button has all the commands related to clans.
 <b>Players</b> - This button has all the commands related to clashers.
-<b>CWL</b> - This button has all the commands related to leagues. (Coming Soon)
+<b>CWL</b> - This button has all the commands related to leagues. <i>(Coming Soon)</i>
 '''
 
 clan_button_message = '''
 <b>CLANS</b>
 
 Here we have all the commands related to clans-
+
+/getClan #{clan tag}: To get a clan.
+/getClanDetails #{clan tag}: To get details of a clan.
+
 <b><i>Coming Soon</i></b>
 '''
 
@@ -33,4 +37,27 @@ player_button_message = '''
 
 Here we have all the commands related to clashers-
 <b><i>Coming Soon</i></b>
+'''
+
+clan_message = '''
+Clan Name: {}
+Clan Level: {}
+Clan Direct link: {}
+'''
+
+clan_details_message = '''
+Clan Name: {}
+Clan Level: {}
+Clan Description: {}
+Clan Location: {}
+Clan Points: {}
+Clan Versus Points: {}
+Clan Type: {}
+Clan Required Trophies: {}
+Clan Members: {}
+Clan War League: {}
+Clan War Frequency: {}
+Clan War Wins: {}
+Clan War Loss: {}
+Clan War Streak: {}
 '''
